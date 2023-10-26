832b76d6bfdab10aec9e6803c043d277c4f8a658
Artemiy <artem.lyamin3018@yandex.ru>
2023-10-26 23:39:05 +0300

    feat: Delete normalize and random rotation in transforms

--- src/dataset/process/transforms.py
+++ src/dataset/process/transforms.py
@@ -3,9 +3,9 @@ from torchvision import transforms
 
 train_transforms = transforms.Compose([
     transforms.ToTensor(),
-    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+    # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
     transforms.RandomHorizontalFlip(p=1),
-    transforms.RandomRotation(degrees=30),
+    # transforms.RandomRotation(degrees=30),
     # transforms.ColorJitter(brightness=(0.1, 0.6), contrast=1, saturation=0, hue=0.4)
 ])
 
