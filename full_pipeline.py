36d9a700eed175833a971cb1b53e0faa020298e2
Artemiy <artem.lyamin3018@yandex.ru>
2023-10-26 23:35:22 +0300

    feat: Parse images if required

--- full_pipeline.py
+++ full_pipeline.py
@@ -1,11 +1,14 @@
 from src.parser.main_parser import main_parser
 from src.dataset.main_dataset import main_dataset
 from src.logging.logger import init_logger, get_logger
+from src.config.config import config
 
 
 if __name__ == "__main__":
     init_logger()
     logger = get_logger()
 
-    parser = main_parser()
+    if config["parse"]:
+        parser = main_parser()
+
     train_dataset = main_dataset()
