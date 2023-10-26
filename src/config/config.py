5c8797c07a04ea1b2be73e30f5a3f4038999ef5a
Artemiy <artem.lyamin3018@yandex.ru>
2023-10-26 23:34:02 +0300

    feat: Add learning parameters in config

--- src/config/config.py
+++ src/config/config.py
@@ -1,9 +1,12 @@
 from datetime import datetime
 from pathlib import Path
 
+import torch.cuda
 
 date_today = datetime.today()
 date_str = f"{date_today.year}{date_today.month}{date_today.day}_{date_today.hour}_{date_today.minute}"
+
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 config = dict(
@@ -13,5 +16,13 @@ config = dict(
     PATH_LOGS=Path("./logs/"),
     LOG_FILE=f"log_{date_str}.log",
 
-    RESCALE_SIZE=224
+    parse=False,
+
+    RESCALE_SIZE=64,
+    LATENT_SIZE=16,
+
+    MAX_EPOCHS=20,
+    BATCH_SIZE=4,
+
+    DEVICE=DEVICE,
 )
